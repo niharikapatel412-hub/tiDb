6e7ec091e4c4918f13e3d24f1c43b4ff6699fa0e
niharikap1 <niharikapatel412@gmail.com>
2025-09-11 21:36:57 +0100

    initialized repo

---
+++ tiDB/src/main/java/com/example/crud/tiDB/TiDbCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud.tiDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiDbCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiDbCrudExampleApplication.class, args);
+	}
+
+}
---
+++ tiDB/src/test/java/com/example/crud/tiDB/TiDbCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud.tiDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiDbCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
