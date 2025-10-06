7e4bca21ef4878f2aec7f5a468658baf1ec8c929
niharikap1 <niharikapatel412@gmail.com>
2025-10-07 00:28:26 +0100

    Chat Memory added

---
+++ tiDB/src/main/java/com/example/crud/tiDB/config/ChatMemoryConfig.java
@@ -0,0 +1,31 @@
+package com.example.crud.tiDB.config;
+
+import org.springframework.ai.chat.memory.repository.jdbc.MysqlChatMemoryRepositoryDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+
+
+import javax.sql.DataSource;
+
+@Configuration
+public class ChatMemoryConfig {
+
+    @Bean
+    public JdbcChatMemoryRepository jdbcChatMemoryRepository(DataSource dataSource) {
+        return JdbcChatMemoryRepository.builder()
+                .dataSource(dataSource)
+                .dialect(new MysqlChatMemoryRepositoryDialect()) // works for TiDB
+                .build();
+    }
+
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository repository) {
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(repository)
+                .maxMessages(20) // memory window
+                .build();
+    }
+}
--- tiDB/src/main/java/com/example/crud/tiDB/controller/AgentController.java
+++ tiDB/src/main/java/com/example/crud/tiDB/controller/AgentController.java
@@ -1,28 +1,61 @@
 package com.example.crud.tiDB.controller;
 
+
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.web.bind.annotation.*;
-
-import javax.sql.DataSource;
 
 @RestController
 @RequestMapping("/agent")
 public class AgentController {
 
     private final ChatClient chatClient;
+    private final ChatMemory chatMemory;
+    private final JdbcChatMemoryRepository chatMemoryRepository;
 
-    public AgentController(ChatClient.Builder builder, ToolCallbackProvider toolCallbackProvider) {
+    public AgentController(ChatClient.Builder builder, ToolCallbackProvider toolCallbackProvider, ChatMemory chatMemory, JdbcChatMemoryRepository chatMemoryRepository) {
+
+        this.chatMemory = chatMemory;
+        this.chatMemoryRepository = chatMemoryRepository;
         this.chatClient = builder
+                .defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .defaultToolCallbacks(toolCallbackProvider)
                 .build();
     }
 
+//    @GetMapping("/ask")
+//    public String ask(@RequestParam String prompt,
+//                      @RequestParam(defaultValue = "default-session") String sessionId
+//    JdbcChatMemoryRepository chatMemoryRepository) {
+//
+//        MessageChatMemoryAdvisor advisor = MessageChatMemoryAdvisor.builder(chatMemory)
+//                .conversationId(sessionId)
+//                .build();
+//        return chatClient
+//                .prompt()
+//                .advisors(advisor)
+//                .user(prompt)
+//                .call()
+//                .content();
+//    }
 
     @GetMapping("/ask")
-    public String ask(@RequestParam String prompt) {
+    public String ask(@RequestParam String prompt,
+                      @RequestParam(defaultValue = "default-session") String sessionId) {
+
+        var chatMemory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(chatMemoryRepository)
+                .maxMessages(20)
+                .build();
+
         return chatClient
                 .prompt()
+                .advisors(MessageChatMemoryAdvisor.builder(chatMemory).conversationId(sessionId).build())
                 .user(prompt)
                 .call()
                 .content();
@@ -42,3 +75,8 @@ public class AgentController {
         return response;
     }
 }
+
+
+
+
+
